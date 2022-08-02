09cc3c289eb4163aa9e646c594a27d7bed419cde
CiaranJRobinson <ciaranjayrobinson@gmail.com>
2022-08-02 17:27:06 -0400

    finished

--- lab.js
+++ lab.js
@@ -381,13 +381,25 @@ console.log(helensInfo)
 
 //Code Here 
 
+class Vehicle {
+  constructor (capacity , color , mileage) {
+    this.capacity = capacity;
+    this.color = color;
+    this.mileage = mileage;
+  }
+
+  move(miles) {
+    this.mileage = this.mileage + miles;
+  console.log(this.mileage);
+  }
+
+}
 
 /*
   Create a vehicle using your new class and save it to a variable called myFirstVehicle
 */
 
-//Code Here
-
+let myFirstVehicle = new Vehicle (4, "blue", 0);
 
 /* 
   Now we'll create a class that's based off of the vehicle class. 
@@ -397,18 +409,23 @@ console.log(helensInfo)
   new ones: make and isCool. (Hint: don't forget to call the super function)
 */
 
-//Code Here
-
+class Motorcycle extends Vehicle {
+  constructor(make , isCool, capacity, color, mileage) {
+    super(capacity, color, mileage)
+    this.make = make;
+    this.isCool = false;
+  }
+}
 /*
   Create a Motorcycle using your new class and save it to a variable called myFirstMotorcycle
 */
 
-//Code Here 
+let myFirstMotorcycle = new Motorcycle ("ghf", true, 1, "red", 1000);
 
 /*
   Call the move function on myFirstMotorcycle (don't forget the parameter)
 */
-
+myFirstMotorcycle.move(5);
 /*
   Let's make another class based off of Vehicle. 
 
@@ -425,7 +442,27 @@ console.log(helensInfo)
   This function should set isSeaworthy to be true
 */
 
-//Code Here
+class Boat extends Vehicle {
+  constructor(name , type , isSeaworthy , capacity , color , mileage) {
+    super(capacity , color , mileage)
+    this.name = name,
+    this.type = type,
+    this.isSeaworthy = isSeaworthy
+  }
+
+  checkSeaWorthiness() {
+    if (this.isSeaworthy === true) {
+      console.log(`The ${this.color} ${this.type} ${this.name} is seaworthy!`);
+    } else {
+      console.log(`You need to get your ${this.type} in shape!`);
+    }
+  }
+
+  performMaintenance() {
+    this.isSeaworthy = true;
+  }
+}
+
 
 
 /*
@@ -433,22 +470,21 @@ console.log(helensInfo)
   properties except isSeaworthy -- make that one false. Call your variable myFirstBoat.
 */
 
-//Code Here
+let myFirstBoat = new Boat ("Floaty", "Speedy", false, 7, "white sparkles", 300);
 
 /*
   Call the checkSeaworthiness method on your new boat
 */
 
-//Code Here
-
+myFirstBoat.checkSeaWorthiness();
 /*
   Now run the performMaintenance method on your boat
 */
 
-//Code Here 
+myFirstBoat.performMaintenance();
 
 /*
   Check the seaworthiness once more (you should be ready for the water!)
 */
 
-//Code Here
+myFirstBoat.checkSeaWorthiness();
